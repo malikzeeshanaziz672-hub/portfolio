1bd6fbea36a55e07e5f5d7793eea8445ccb0ae33
Zeeshan Aziz <malikzeeshanaziz672@gmail.com>
2025-08-19 19:46:06 +0500

    work on new updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import React, { Suspense } from 'react'
-import "./App.css"
-import Loader from './Components/Loader'
-import Router from './libs/Router'
-const App = () => {
-  return (
-    <Suspense fallback={Loader}>
-     <Router/>
-    </Suspense>
-  )
-}
+import React from "react";
 
-export default App
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
